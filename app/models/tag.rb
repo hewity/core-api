efc1210c04a1c593a8fa473ed1669a6ff6b41b2f
Hewan Abreha <henebys@gmail.com>
2016-05-09 10:58:47 -0400

    add comments model and controller

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ActiveRecord::Base
+has_many :posts
+has_many :links
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
   resources :posts
   resources :tags
   resources :links
