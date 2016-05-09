602daf599acd25f8403134f37ffaa13e16f65ad4
Spicy Legato <jpqreichard@gmail.com>
2016-05-09 09:57:36 -0400

    adds allowance for CORS reqeusts

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,6 +22,13 @@ module CoreApi
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
