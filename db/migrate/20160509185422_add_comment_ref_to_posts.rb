43932e5da5e435a2b6cde3d8ffb20ef71b08d05b
Hewan Abreha <henebys@gmail.com>
2016-05-09 15:07:46 -0400

    update references to comment

--- db/migrate/20160509185422_add_comment_ref_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddCommentRefToPosts < ActiveRecord::Migration
-  def change
-    add_reference :posts, :comment, index: true, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509185422) do
+ActiveRecord::Schema.define(version: 20160509190458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20160509185422) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "links", force: :cascade do |t|
     t.text     "about"
@@ -45,5 +48,6 @@ ActiveRecord::Schema.define(version: 20160509185422) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "comments"
 end
