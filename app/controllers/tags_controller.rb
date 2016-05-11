b00261a1c71d98e4da39d76b3963eb67ab7307d2
Spicy Legato <jpqreichard@gmail.com>
2016-05-11 17:47:09 -0400

    renders index as json, includes children resources with tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,36 +1,36 @@
 class TagsController < ApplicationController
   def index
     @tags = Tag.all
-    render json: @tags
+    render json: @tags.to_json(include: [:links, :posts])
   end
 
   def show
     @tag = Tag.find(params[:id])
-    render status: 200, json: @tag.to_json
+    render status: 200, json: @tag.to_json(include: [:links, :posts])
   end
 
   def create
     @tag = Tag.new(tag_params)
     if @tag.save
-      render status: 200, json: @tag.to_json
+      render status: 200, json: @tag.to_json(include: [:links, :posts])
     end
   end
 
   def update
     @tag = Tag.find(params[:id])
     if @tag.update(tag_params)
-      render status: 200, json: @tag.to_json
+      render status: 200, json: @tag.to_json(include: [:links, :posts])
     end
   end
 
   def destroy
     @tag = Tag.find(params[:id])
     if @tag.destroy
-      render status: 200, json: @tag.to_json
+      render status: 200, json: @tag.to_json(include: [:links, :posts])
     end
 
     private
     def tag_params
-      params.require(:tag).permit(:name)
+      params.require(:tag).permit(:name, :post_id, :link_id)
     end
   end
