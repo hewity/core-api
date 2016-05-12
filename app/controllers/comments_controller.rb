b22e30344727fc674b43ccd33d86eb36568a786f
Wes Reed <wesley.a.reed@gmail.com>
2016-05-12 10:40:27 -0400

    Update comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,6 +27,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:body, :author, :post_id)
+    params.require(:comment)
+    .permit(:body, :author, :post_id)
   end
 end
